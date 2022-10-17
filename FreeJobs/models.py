ce8e3306c5bb3b25d9ff812b2fc419bd2182db2d
Ruben-Abiasaf-Quintana-Camacho-2015010863 <76923860+Ruben-Quintana-Camacho-2015010863@users.noreply.github.com>
2022-10-17 11:07:00 -0500

    Creacion de tablas como clases

--- FreeJobs/models.py
+++ FreeJobs/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Usuario(){
+class User(models.Model):
+    userID = models.AutoField(primary_key = True)
+    userName = models.CharField(max_length = 100)
+    lastName = models.CharField(max_length = 100)
+    '''usename'''
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    '''code'''
+    isActive = models.BoolField() '''checar declaracion'''
+    isAdmin = models.BoolField()
+    isOffer = models.BoolField()
+    createdAt = models.DateField()
 
-}
+class Profile(models.Model):
+    birth = models.DateField()
+    gender = models.CharField(max_length = 1)
+    
